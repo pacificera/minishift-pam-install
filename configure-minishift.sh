d5c3294e86f06c80a09e679decd9427e1afc30d4
David Williams <dww@pacificera.com>
2018-09-06 16:09:47 -0600

    Added properties files

--- configure-minishift.sh
+++ configure-minishift.sh
@@ -1,7 +1,15 @@
 #/bin/bash
+source configure-minishift.properties
+
+echo ${memory}
+echo ${cpus}
 
 # create profile
 minishift profile set rhpamauthenv
+
+# set profile resources
+minishift config set memory ${memory}
+minishift config set cpus ${cpus}
 
 # install admin add on
 # this allows the pam product containers to be installed in the openshift space
--- install-pam.sh
+++ install-pam.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-# set configuration variables
-export PROC_SERVER_PASSWORD=pspass
-export BUS_CENTRAL_PASSWORD=bcpass
-export KIE_ADMIN_USER=admin
-export KIE_ADMIN_PWD=admin
-export PROJ_NAME=rhpamauthenv
-export OC_PAM_TEMPLATES_DIR=rhpam-7.0.2-openshift-templates
-
+source install-pam.properties
 
 # login admin to setup imagestreams in openshift project
 oc login --username=admin --password=admin --insecure-skip-tls-verify=true
 
 # install PAM image streams
 oc project openshift
-oc create -f rhpam-7.0.2-openshift-templates/rhpam70-image-streams.yaml
+oc create -f ${OC_PAM_TEMPLATES_DIR}/${OC_PAM_IMAGE_STREAMS}
 
 # login as developer to setup pam
 oc login --username=developer  --password=developer --insecure-skip-tls-verify=true
 
 # creae the pam project within openshift
-oc new-project $PROJ_NAME
+oc new-project ${PROJECT_NAME}
 
 # create process server cert
 mkdir -p certs/procserver
 cd certs/procserver
 rm keystore.jks
-keytool -genkeypair -alias jboss -keyalg RSA -keystore keystore.jks -storepass $PROC_SERVER_PASSWORD -keypass $PROC_SERVER_PASSWORD\
+keytool -genkeypair -alias jboss -keyalg RSA -keystore keystore.jks -storepass ${PROC_SERVER_PASSWORD} -keypass ${PROC_SERVER_PASSWORD} \
 	--dname "CN=jsmith,OU=Engineering,O=mycompany.com,L=Raleigh,S=NC,C=US"
 
 oc delete secret kieserver-app-secret
@@ -37,7 +30,7 @@ cd ../..
 mkdir -p certs/buscentral
 cd certs/buscentral
 rm keystore.jks
-keytool -genkeypair -alias jboss -keyalg RSA -keystore keystore.jks -storepass $BUS_CENTRAL_PASSWORD -keypass $BUS_CENTRAL_PASSWORD\
+keytool -genkeypair -alias jboss -keyalg RSA -keystore keystore.jks -storepass ${BUS_CENTRAL_PASSWORD} -keypass ${BUS_CENTRAL_PASSWORD} \
 	--dname "CN=jsmith,OU=Engineering,O=mycompany.com,L=Raleigh,S=NC,C=US"
 
 oc delete secret businesscentral-app-secret
@@ -45,13 +38,13 @@ oc create secret generic businesscentral-app-secret --from-file=keystore.jks
 cd ../..
 
 # create the app
-oc new-app -f $OC_PAM_TEMPLATES_DIR/templates/rhpam70-authoring.yaml \
-   -p KIE_ADMIN_USER=$KIE_ADMIN_USER \
-   -p KIE_ADMIN_PWD=$KIE_ADMIN_PWD \
+oc new-app -f ${OC_PAM_TEMPLATES_DIR}/${OC_PAM_APP_TEMPLATE} \
+   -p KIE_ADMIN_USER=${KIE_ADMIN_USER} \
+   -p KIE_ADMIN_PWD=${KIE_ADMIN_PWD} \
    -p BUSINESS_CENTRAL_HTTPS_SECRET=businesscentral-app-secret \
    -p KIE_SERVER_HTTPS_SECRET=kieserver-app-secret \
-   -p APPLICATION_NAME=$PROJ_NAME \
+   -p APPLICATION_NAME=${APPLICATION_NAME} \
    -p BUSINESS_CENTRAL_HTTPS_NAME=jboss \
    -p KIE_SERVER_HTTPS_NAME=jboss \
-   -p BUSINESS_CENTRAL_HTTPS_PASSWORD=$BUS_CENTRAL_PASSWORD \
-   -p KIE_SERVER_HTTPS_PASSWORD=$PROC_SERVER_PASSWORD 
+   -p BUSINESS_CENTRAL_HTTPS_PASSWORD=${BUS_CENTRAL_PASSWORD} \
+   -p KIE_SERVER_HTTPS_PASSWORD=${PROC_SERVER_PASSWORD} 
